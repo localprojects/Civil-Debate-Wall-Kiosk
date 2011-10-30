ebf2396ffb652bfa00b81dbd7cc8c93dfccd4df2
ericmika <ericmika@localprojects.net>
2011-10-30 17:15:11 -0400

    Fix line endings in batch file.

--- air_projects/cdw_kiosk/build.bat
+++ air_projects/cdw_kiosk/build.bat
@@ -5,7 +5,6 @@ git reset --hard HEAD
 git pull
 
 echo Building and uploading install file..
-
 set WORKSPACE=%HOMEPATH%\Desktop\CDWBuild\lp-cdw\air_projects
 
 "C:\Program Files (x86)\Adobe\Adobe Flash Builder 4.5\FlashBuilderC.exe" ^
