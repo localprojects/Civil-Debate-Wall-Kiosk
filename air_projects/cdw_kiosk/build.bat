377d8bca20f96904e9d7e41c10032e10e9e85e6d
Eric Mika <ericmika@localprojects.net>
2011-12-15 02:45:26 -0500

    Build Fix for Flash Builder 4.6

--- air_projects/cdw_kiosk/build.bat
+++ air_projects/cdw_kiosk/build.bat
@@ -2,7 +2,7 @@ echo Running CDW Build script...
 
 set WORKSPACE=%HOMEPATH%\Desktop\CDWBuild\lp-cdw\air_projects
 
-"C:\Program Files (x86)\Adobe\Adobe Flash Builder 4.5\FlashBuilderC.exe" ^
+"C:\Program Files (x86)\Adobe\Adobe Flash Builder 4.6\FlashBuilderC.exe" ^
 	--launcher.suppressErrors ^
 	-noSplash ^
 	-application org.eclipse.ant.core.antRunner ^
